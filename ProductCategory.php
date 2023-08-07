ad5895d7a5738487bc66122451cc9e33074b6984
11010127 <105908669+11010127@users.noreply.github.com>
2023-08-08 05:43:12 +0800

    ProductCategory.php目錄
    增加產品類別目錄，修正ProductCategoryR.php錯誤內容。

---
+++ ProductCategory.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新增</title>
+    <link rel="stylesheet" href="ProductCategoryCSS.css">
+  </head>
+  <body>
+    <div class="titleBox"><h1>產品類別</h1></div>
+
+    <div class="aBox">
+      <a href="ProductCategoryC.php">新增</a>
+      <a href="ProductCategoryU.php">修改</a>
+      <a href="ProductCategoryR.php">查詢</a>
+      <a href="ProductCategoryD.php">刪除</a>
+    </div>
+    
+    </form>
+  </body>
+</html>
