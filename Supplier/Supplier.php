3f09e37aaff2d4c93b3c4e1263a2149be82849c2
11010127 <105908669+11010127@users.noreply.github.com>
2023-08-08 05:50:27 +0800

    供應商主要頁面
    增加供應商主要頁面Supplier.php

---
+++ Supplier/Supplier.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新增</title>
+    <link rel="stylesheet" href="SupplierCSS.css">
+    <style>
+      .contentBox{
+        height: 400px;
+
+      }
+    </style>
+  </head>
+  <body>
+    <div class="titleBox"><h1>供應商</h1></div>
+
+    <div class="aBox">
+      <a href="SupplierC.php">新增</a>
+      <a href="SupplierU.php">修改</a>
+      <a href="SupplierR.php">查詢</a>
+      <a href="SupplierD.php">刪除</a>
+    </div>
+
+    
+    </form>
+  </body>
+</html>
